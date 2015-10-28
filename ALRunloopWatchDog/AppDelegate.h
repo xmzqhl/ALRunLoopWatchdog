5fbc197aebc29366e7bc1fca7fc1f3a78553b675
Arien Lau <arien@ariendemacbook-pro.local>
2015-10-28 10:10:46 +0800

    Initial Commit

---
+++ ALRunloopWatchDog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALRunloopWatchDog
+//
+//  Created by Arien Lau on 15/10/28.
+//  Copyright © 2015年 Arien Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
